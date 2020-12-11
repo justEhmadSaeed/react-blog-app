e3121c383f7903da2abe891df0577838e3f1b3af
Ehmad Saeed <justehmadsaeed@gmail.com>
2020-12-12 02:23:31 +0500

    📦 NEW: Add navbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import logo from "./logo.svg";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import ArticleList from "./pages/ArticleList";
+import ArticlePage from "./pages/ArticlePage";
+import AboutPage from "./pages/AboutPage";
+import NavBar from "./NavBar";
 
 function App() {
 	return (
 		<Router>
-			<div className="App">
-				<Route path="/" component={HomePage} exact />
+      <div className="App">
+        <NavBar/>
+				<div id="page-body">
+					<Route path="/" component={HomePage} exact />
+					<Route path="/about" component={AboutPage} />
+					<Route path="/article-list" component={ArticleList} />
+					<Route path="/article" component={ArticlePage} />
+				</div>
 			</div>
 		</Router>
 	);
