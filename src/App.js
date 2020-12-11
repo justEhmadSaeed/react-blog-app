e978d94824efa00c34c0f066941b626a1285d92d
Ehmad Saeed <justehmadsaeed@gmail.com>
2020-12-12 01:24:28 +0500

    📦 NEW: Add Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<Route path="/" component={HomePage} exact/>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
