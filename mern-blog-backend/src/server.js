ae6675d501aa86cd5d9de0cd2b815722232a30bd
Ehmad Saeed <justehmadsaeed@gmail.com>
2020-12-22 23:41:44 +0500

    📦 NEW: Upvote article API

--- mern-blog-backend/src/server.js
+++ mern-blog-backend/src/server.js
@@ -1,13 +1,26 @@
-import bodyParser from "body-parser"
 import express from "express"
 
 const app = express()
+
+const articleInfo = {
+	"learn-react": {
+		upvotes: 0,
+	},
+	"learn-node": {
+		upvotes: 0,
+	},
+	"my-thoughts-on-resumes": {
+		upvotes: 0,
+	},
+}
 app.use(express.json())
 
-app.get("/hello", (req, res) => res.send("Hello!"))
-
-app.get("hello/:name", (req, res) => res.send(`Hello ${req.params.name}`))
-
-app.post("/hello", (req, res) => res.send(`Hello ${req.body.name}`))
+app.post("/api/articles/:name/upvote", (req, res) => {
+	const articleName = req.params.name
+	articleInfo[articleName].upvotes++
+	res
+		.status(200)
+		.send(`${articleName} now has ${articleInfo[articleName].upvotes} upvotes.`)
+})
 
 app.listen(8000, () => console.log("Listening on Port 8000"))
