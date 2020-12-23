3ac955b2fbc913503571ce1ef60ebbc7eb7d1847
Ehmad Saeed <justehmadsaeed@gmail.com>
2020-12-24 00:24:35 +0500

    📦 NEW: Add Comment Api

--- mern-blog-backend/src/server.js
+++ mern-blog-backend/src/server.js
@@ -5,12 +5,15 @@ const app = express()
 const articleInfo = {
 	"learn-react": {
 		upvotes: 0,
+		comments: [],
 	},
 	"learn-node": {
 		upvotes: 0,
+		comments: [],
 	},
 	"my-thoughts-on-resumes": {
 		upvotes: 0,
+		comments: [],
 	},
 }
 app.use(express.json())
@@ -22,5 +25,10 @@ app.post("/api/articles/:name/upvote", (req, res) => {
 		.status(200)
 		.send(`${articleName} now has ${articleInfo[articleName].upvotes} upvotes.`)
 })
-
+app.post("/api/articles/:name/comment", (req, res) => {
+	const { username, comment } = req.body
+	const articleName = req.params.name
+	articleInfo[articleName].comments.push({ username, comment })
+	res.status(200).send(articleInfo[articleName])
+})
 app.listen(8000, () => console.log("Listening on Port 8000"))
